2b2c161bcf7461ae8c6844246f5be8bd0ad0a2c7
Ta-Seen Islam <taseen00.islam@gmail.com>
2022-05-17 16:31:02 +1000

    Add custom home page

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,78 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
 import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
 
-const Layout: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
-};
+const NAME = "John Smith";
+export const SITE_TITLE = "Next.js Sample Website";
 
-export default Layout;
+export type LayoutProps = React.PropsWithChildren<{
+  home?: boolean;
+}>;
+
+export default function Layout({ children, home = false }: LayoutProps) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website with Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            SITE_TITLE
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={SITE_TITLE} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt={NAME}
+            />
+            <h1 className={utilStyles.heading2Xl}>{NAME}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <Image
+                  priority
+                  src="/images/profile.jpg"
+                  className={utilStyles.borderCircle}
+                  height={108}
+                  width={108}
+                  alt={NAME}
+                />
+              </a>
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/">
+                <a className={utilStyles.colorInherit}>{NAME}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>&lt; Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
