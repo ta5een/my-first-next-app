76286322bc5d8a2d0ca3910dbde1627d40a0c2d2
Ta-Seen Islam <taseen00.islam@gmail.com>
2022-05-17 18:23:57 +1000

    Add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/layout";
+
+export default function FourOFour() {
+  return (
+    <Layout>
+      <h1>Page Not Found</h1>
+      <p>Sorry, the page you were looking for could not be found.</p>
+    </Layout>
+  );
+}
